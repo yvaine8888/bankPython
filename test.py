fc724486508febb7734b27c245f450779d217232
Yvaine Liew <yvaine8888@gmail.com>
2025-04-21 22:12:22 -0500

    fixed tests

--- test.py
+++ test.py
@@ -3,40 +3,40 @@ from main import*
 from unittest.mock import patch
 
 class MyTests(unittest.TestCase):
-    def a_delete(self):
+    def test_adelete(self):
         self.assertTrue(delete(316184))
     
-    def b_create(self):
+    def test_bcreate(self):
         self.assertTrue(create('bliew', 15, 316184))
 
-    def c_balance(self):
+    def test_cbalance(self):
         self.assertEqual(check_balance('bliew'), {316184: 15})
     
-    def d_deposit(self):
+    def test_deposit(self):
         self.assertTrue(change_balance(316184, 30))
 
-    def e_withdraw(self):
+    def test_ewithdraw(self):
         self.assertTrue(change_balance(316184, -30))
     
-    def f_modify_name():
+    def test_fmodify_name(self):
         with patch('builtins.input') as mock_input:
             mock_input.side_effect = ['name', 'Yvaine', 'N']
             result = modify('bliew')
             self.assertEqual(result, 'bliew')
 
-    def g_modify_name():
+    def test_modify_name(self):
         with patch('builtins.input') as mock_input:
             mock_input.side_effect = ['name', 'Bethany', 'Y', 'name', 'Bethany', 'N']
             result = modify('bliew')
             self.assertEqual(result, 'bliew')
 
-    def g_modify_username():
+    def test_modify_username(self):
         with patch('builtins.input') as mock_input:
-            mock_input.side_effect = ['username', 'yliew', 'Y', 'username', 'bliew', 'N']
+            mock_input.side_effect = ['username', 'tliew', 'Y', 'username', 'bliew', 'N']
             result = modify('bliew')
             self.assertEqual(result, 'bliew')
     
-    def g_modify_password():
+    def test_modify_password(self):
         with patch('builtins.input') as mock_input:
             mock_input.side_effect = ['password', '456', 'Y', 'password', '123', 'N']
             result = modify('bliew')
